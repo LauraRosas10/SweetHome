1ed6d25fe9c6b3b71216b0a6fe5c1e3cf614af34
LauraRosas10 <165853126+LauraRosas10@users.noreply.github.com>
2025-10-20 15:19:31 -0500

    SecurityConfig

--- Backend/src/main/java/uis/edu/entorno/backend/config/SecurityConfig.java
+++ Backend/src/main/java/uis/edu/entorno/backend/config/SecurityConfig.java
@@ -22,11 +22,11 @@ public class SecurityConfig {
         this.jwtProtegidas = jwtProtegidas;
     }
     
-    //cuando se activa segurityconfig apesar de tener el cors es necesario ponerlo aqui
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        // 🎯 Incluye tus orígenes
+  
         configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5500", "http://localhost:5500"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
@@ -39,25 +39,20 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
-                
-                // 1. 🔑 ACTIVAR EL CORS: Esto le indica a Spring Security que use el bean de arriba
+        
                 .cors(Customizer.withDefaults()) 
                 
                 .authorizeHttpRequests(auth -> auth
                         
-                        // 2. 🔑 PERMITIR OPTIONS: Regla crítica para el preflight de CORS.
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() 
                         
-                        // 3. RUTAS PÚBLICAS: GET /producto/** no requiere token.
                         .requestMatchers(HttpMethod.GET, "/producto/**").permitAll() 
                         
-                        // 4. RUTAS PÚBLICAS: Login y registro no requieren token.
+
                         .requestMatchers("/usuario/login", "/usuario/nuevo","/categoria/").permitAll()
-                        
-                        // 5. El resto de solicitudes (POST/PUT/DELETE, o GET a otras rutas) debe estar autenticado.
+                
                         .anyRequest().authenticated()
                 )
-                // 6. El filtro JWT se aplica antes de que Spring evalúe las credenciales de usuario y solo para rutas que requieren autenticación.
                 .addFilterBefore(jwtProtegidas, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
